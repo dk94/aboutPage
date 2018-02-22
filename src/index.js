d646300cb058dc01b997d1d370db549a144698cc
Dmytro Kyiashko <dmytro.kyiashko@gmail.com>
2018-02-22 19:55:43 +0200

    Add basic styles

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// import 'normalize.css';
+import styles from './less/styles.less';
+import footer from './less/footer.less';
+import grid from './less/grid.less';
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "less-loader", options: {
+            strictMath: true,
+            noIeCompat: true
+          }
+        }]
+      }
+    ]
   }
 };
